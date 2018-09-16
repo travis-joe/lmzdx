11f69cc8f61eb30179de8089c39f3f2bb3a3d50e
qzy <weelgod@gmail.com>
2018-09-16 14:07:43 +0800

    bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = '绿帽子大学'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
